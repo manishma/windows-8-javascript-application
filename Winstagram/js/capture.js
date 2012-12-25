5874ff6d86ceb323d4ac2d8d5ecb1c82de9f51c0
Igor Zelmanovich <igor.israel@gmail.com>
2012-12-25 11:53:07 +0200

    take a picture

--- Winstagram/js/capture.js
+++ Winstagram/js/capture.js
@@ -3,20 +3,23 @@
 
     WinJS.UI.Pages.define("/capture.html", {
         ready: function (element, options) {
+            btn_capture.addEventListener("click", function() {
+                takePhoto();
+            });
             startCamera();
         }
     });
 
     // Using
     var Capture = Windows.Media.Capture;
-    //var Storage = Windows.Storage;
+    var Storage = Windows.Storage;
 
     //// Globals
-    //var mediaCapture;
+    var mediaCapture;
     //var recording = false;
     //var recordedFile;
     
-    //var pics;
+    var pics;
     //var videos;
     //var flipView;
     //var imageProcessor;
@@ -25,11 +28,28 @@
 
     function startCamera() {
         var livePreview = document.getElementById("live-preview");
-        var mediaCapture = new Capture.MediaCapture();
+        mediaCapture = new Capture.MediaCapture();
         mediaCapture.initializeAsync().then(function () {
             livePreview.src = URL.createObjectURL(mediaCapture);
             livePreview.play();
         });
     }
 
+    var picturesLib = Storage.KnownFolders.picturesLibrary;
+    picturesLib.createFolderAsync("Winstagram", Storage.CreationCollisionOption.openIfExists)
+      .then(function (folder) {
+          pics = folder;
+      });
+    
+    function takePhoto() {
+        pics.createFileAsync((new Date()).getTime()+".jpg", Storage.CreationCollisionOption.generateUniqueName)
+          .then(function (file) {
+              var photoProperties = Windows.Media.MediaProperties.ImageEncodingProperties.createJpeg();
+
+              mediaCapture.capturePhotoToStorageFileAsync(photoProperties, file).then(function () {
+                  console.log("Image saved on disk on: " + file.path);
+              });
+          });
+    }
+    
 })();
