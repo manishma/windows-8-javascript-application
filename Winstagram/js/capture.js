9a25787d95a0729f5dff0eb4d25b86b5b0e4231e
Igor Zelmanovich <igor.israel@gmail.com>
2012-12-25 10:29:26 +0200

    working on capture page

--- Winstagram/js/capture.js
+++ Winstagram/js/capture.js
@@ -1,1 +1,28 @@
-﻿
+﻿(function() {
+
+    // Using
+    var Capture = Windows.Media.Capture;
+    //var Storage = Windows.Storage;
+
+    //// Globals
+    //var mediaCapture;
+    //var recording = false;
+    //var recordedFile;
+    
+    //var pics;
+    //var videos;
+    //var flipView;
+    //var imageProcessor;
+    //var screensList;
+    
+
+    function startCamera() {
+        var livePreview = document.getElementById("live-preview");
+        var mediaCapture = new Capture.MediaCapture();
+        mediaCapture.initializeAsync().then(function () {
+            livePreview.src = URL.createObjectURL(mediaCapture);
+            livePreview.play();
+        });
+    }
+
+})();
