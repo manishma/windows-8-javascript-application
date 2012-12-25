92f743762277ffdac1149669301d24ea54215f3e
Menny Rachmanny <onemenny@gmail.com>
2012-12-25 14:43:09 +0200

    adding canvas as image

--- WindowsRuntimeComponent.ImageProcessing/ImageManager.cs
+++ WindowsRuntimeComponent.ImageProcessing/ImageManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
 
 namespace WindowsRuntimeComponent.ImageProcessing
 {
@@ -10,7 +11,31 @@ namespace WindowsRuntimeComponent.ImageProcessing
     {
         public void SetTextToImage(string path, string textToAdd)
         {
+            //Windows.Graphics.Imaging.
+            //BitmapEncoder.
+            //var bmp = Bitmap.FromFile("orig.jpg");
+            //var newImage = new Bitmap(bmp.Width, bmp.Height + 50);
 
+            //var gr = Graphics.FromImage(newImage);
+            //gr.DrawImageUnscaled(bmp, 0, 0);
+            //gr.DrawString("this is the added text", SystemFonts.DefaultFont, Brushes.Black,
+            //    new RectangleF(0, bmp.Height, bmp.Width, 50));
+
+            //newImage.Save("newImg.jpg");
+
+
+            //FileStream fs = new FileStream(@"c:\somepic.gif", FileMode.Open, FileAccess.Read);
+            //Image image = Image.FromStream(fs);
+            //fs.Close();
+
+            //Bitmap b = new Bitmap(image);
+            //Graphics graphics = Graphics.FromImage(b);
+            //graphics.DrawString("Hello", this.Font, Brushes.Black, 0, 0);
+
+            //b.Save(@"c:\somepic.gif", image.RawFormat);
+
+            //image.Dispose();
+            //b.Dispose();
         }
     }
 }
--- Winstagram/js/collage.js
+++ Winstagram/js/collage.js
@@ -22,11 +22,178 @@
         
             document.getElementById("picture-thumb-start").addEventListener("click", pickPhoto, false);
 
-
-            var imageMgr = new WindowsRuntimeComponent.ImageProcessing.ImageManager();
-            imageMgr.setTextToImage("path to image", "this is my image caption");
+            onLoad();
+            //var imageMgr = new WindowsRuntimeComponent.ImageProcessing.ImageManager();
+            //imageMgr.setTextToImage("path to image", "this is my image caption");
         }
     });
+
+    var context;
+    var brushList;
+    var canvas;
+    var output;
+    var animationActive = false;
+
+    function onLoad() {
+        var element;
+        canvas = document.getElementById("paintCanvas");
+        output = document.getElementById("output");
+
+        // account for margins
+        canvas.width = output.clientWidth - 25;
+        canvas.height = output.clientHeight - 125;
+
+        context = canvas.getContext("2d");
+        canvas.addEventListener("MSPointerDown", canvasHandler, false);
+        canvas.addEventListener("MSPointerMove", canvasHandler, false);
+        canvas.addEventListener("MSPointerUp", canvasHandler, false);
+        canvas.addEventListener("MSPointerOver", canvasHandler, false);
+        canvas.addEventListener("MSPointerOut", canvasHandler, false);
+        canvas.addEventListener("MSPointerCancel", canvasHandler, false);
+        context.lineWidth = 1;
+        context.lineCap = "round";
+        context.lineJoin = "round";
+
+        initColorPalette();
+        initToolbar();
+
+        brushList = new Array();
+    }
+
+    function initColorPalette() {
+        var element = document.getElementById("palette");
+        var divs = element.getElementsByTagName("div");
+        for (var idx = 0; idx < divs.length; idx++) {
+            divs[idx].addEventListener("MSPointerUp", colorSelector, false);
+        }
+    }
+
+    // Clear canvas for new drawing
+    function initToolbar() {
+        var element = document.getElementById("newFile");
+        element.addEventListener("MSPointerUp", clearCanvas, false);
+    }
+
+    function colorSelector(evt) {
+        context.strokeStyle = evt.srcElement.id;
+        var element = document.getElementById("selectedColor");
+        element.style.backgroundColor = evt.srcElement.id;
+        evt.preventDefault();
+    }
+
+    function brushTool() {
+        var brush = this;
+        brush.started = false;
+        brush.over = false;
+        brush.prevX = 0;
+        brush.prevY = 0;
+        brush.currentX = 0;
+        brush.currentY = 0;
+        brush.lineWidth = 1;
+
+        // Even though the choice of raw coordinates over predicted coordinates has performance
+        // overhead we will use raw coordinates because predicted coordinates don't give
+        // accurate results for our purpose.
+        this.MSPointerDown = function (evt) {
+            canvas.msSetPointerCapture(evt.pointerId);
+            brush.currentX = evt.currentPoint.rawPosition.x;
+            brush.currentY = evt.currentPoint.rawPosition.y;
+            brush.prevX = brush.currentX;
+            brush.prevY = brush.currentY;
+            brush.started = true;
+            brush.over = true;
+            if (!animationActive) {
+                window.requestAnimationFrame(animationHandler);
+                animationActive = true;
+            }
+        };
+
+        this.MSPointerOver = function (evt) {
+            brush.over = true;
+            if (brush.started) {
+                brush.currentX = evt.currentPoint.rawPosition.x;
+                brush.currentY = evt.currentPoint.rawPosition.y;
+            } else if (evt.currentPoint.isInContact) {
+                // If the Down occurred outside of the canvas element but the pointer is in contact,
+                // simulate the Down behavior when the pointer enters the canvas
+                brush.MSPointerDown(evt);
+            }
+        };
+
+        this.MSPointerMove = function (evt) {
+            if (brush.started) {
+                // Adjust the line width by reading the contact width from
+                // the event parameter. Use a width of 1 for pen and mouse.
+                if (evt.pointerType === evt.MSPOINTER_TYPE_TOUCH) {
+                    brush.lineWidth = evt.width / 2;
+                } else {
+                    brush.lineWidth = 1;
+                }
+                brush.currentX = evt.currentPoint.rawPosition.x;
+                brush.currentY = evt.currentPoint.rawPosition.y;
+            }
+        };
+
+        this.MSPointerUp = function (evt) {
+            canvas.msReleasePointerCapture(evt.pointerId);
+            brush.started = false;
+        };
+
+        this.MSPointerOut = function (evt) {
+            brush.over = false;
+        };
+
+        this.MSPointerCancel = function (evt) {
+            brush.over = false;
+            brush.started = false;
+        };
+    }
+
+    function canvasHandler(evt) {
+        var brush;
+        var func;
+
+        if (brushList[evt.pointerId] === null ||
+            brushList[evt.pointerId] === undefined) {
+            brushList[evt.pointerId] = new brushTool();
+        }
+
+        brush = brushList[evt.pointerId];
+
+        func = brush[evt.type];
+        func(evt);
+
+        if (!brush.started && !brush.over) {
+            // clean up when the brush is finished
+            delete brushList[evt.pointerId];
+        }
+    }
+
+    function clearCanvas(evt) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        brushList.length = 0;
+    }
+
+    function animationHandler() {
+        animationActive = false;
+        for (var idx in brushList) {
+            var currentBrush = brushList[idx];
+            if (currentBrush.started) {
+                context.beginPath();
+                context.lineWidth = currentBrush.lineWidth;
+                context.moveTo(currentBrush.prevX, currentBrush.prevY);
+                context.lineTo(currentBrush.currentX, currentBrush.currentY);
+                context.stroke();
+                currentBrush.prevX = currentBrush.currentX;
+                currentBrush.prevY = currentBrush.currentY;
+                animationActive = true;
+            }
+        }
+        if (animationActive) {
+            // Request for another callback until all pointers are gone.
+            window.requestAnimationFrame(animationHandler);
+        }
+    }
 
     function pickPhoto() {
         // Map thumbnail modes to HTML <select> options
@@ -86,12 +253,32 @@
         document.getElementById("picture-thumb-imageHolder").src = URL.createObjectURL(thumbnailImage, { oneTimeOnly: true });
         // Close the thumbnail stream once the image is loaded
         document.getElementById("picture-thumb-imageHolder").onload = function () {
-            thumbnailImage.close();
+
+           // thumbnailImage.close();
         };
         document.getElementById("picture-thumb-modeName").innerText = thumbnailMode;
         document.getElementById("picture-thumb-fileName").innerText = "File used: " + item.name;
         document.getElementById("picture-thumb-requestedSize").innerText = "Requested size: " + requestedSize;
         document.getElementById("picture-thumb-returnedSize").innerText = "Returned size: " + thumbnailImage.originalWidth + "x" + thumbnailImage.originalHeight;
+
+
+
+
+        var can = document.getElementById('paintCanvas');
+        var ctx = can.getContext('2d');
+
+
+
+        var img = new Image();
+        img.onload = function () {
+            can.width = img.width;
+            can.height = img.height;
+            ctx.drawImage(img, 0, 0, img.width, img.height);
+        }
+        img.src = URL.createObjectURL(thumbnailImage, { oneTimeOnly: true });
+
+
+
     }
 
     function cleanOutput() {
